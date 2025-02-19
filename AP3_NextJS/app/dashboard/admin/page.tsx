379b411dfc19daf8a485f7372074e2e0a53f3b17
Esteban-ALLIER <esteban.allier@gmail.com>
2025-02-19 15:47:45 +0100

    test vercel

---
+++ AP3_NextJS/app/dashboard/admin/page.tsx
@@ -0,0 +1,16 @@
+// "use client";
+
+// import { useRef, useState, useEffect } from "react"
+// import { AppSidebar } from "@/components/sidebar/app-sidebar"
+// import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, } from "@/components/ui/breadcrumb"
+// import { Button } from "@/components/ui/button"
+// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+// import { Separator } from "@/components/ui/separator"
+// import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, } from "@/components/ui/dialog"
+// import { SidebarInset, SidebarProvider, SidebarTrigger, } from "@/components/ui/sidebar"
+// import { useAuth } from "@/context/AuthContext"
+// import { Plus } from "lucide-react"
+// import { cn } from "@/lib/utils"
+// import { z } from "zod"
+// import { useToast } from '@/hooks/use-toast';
+
